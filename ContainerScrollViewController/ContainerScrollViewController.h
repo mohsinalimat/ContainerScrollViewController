7e3b9f06d64248fb2941414ddff12ed3ef681871
Drew Olbrich <drew.olbrich@gmail.com>
2018-12-23 15:50:12 -0800

    Add Xcode project

---
+++ ContainerScrollViewController/ContainerScrollViewController.h
@@ -0,0 +1,19 @@
+//
+//  ContainerScrollViewController.h
+//  ContainerScrollViewController
+//
+//  Created by Drew Olbrich on 12/23/18.
+//  Copyright © 2018 Drew Olbrich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ContainerScrollViewController.
+FOUNDATION_EXPORT double ContainerScrollViewControllerVersionNumber;
+
+//! Project version string for ContainerScrollViewController.
+FOUNDATION_EXPORT const unsigned char ContainerScrollViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContainerScrollViewController/PublicHeader.h>
+
+
