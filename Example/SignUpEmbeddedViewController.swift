86e33b9644ac0ffb10050d68b747b6fcfacb8c9a
Drew Olbrich <drew.olbrich@gmail.com>
2018-12-23 21:01:44 -0800

    Add example target

---
+++ Example/SignUpEmbeddedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpEmbeddedViewController.swift
+//  Example
+//
+//  Created by Drew Olbrich on 12/23/18.
+//  Copyright © 2018 Drew Olbrich. All rights reserved.
+//
+
+import UIKit
+
+class SignUpEmbeddedViewController: UIViewController {
+
+}
---
+++ Example/SignUpViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpViewController.swift
+//  Example
+//
+//  Created by Drew Olbrich on 12/23/18.
+//  Copyright © 2018 Drew Olbrich. All rights reserved.
+//
+
+import UIKit
+import ContainerScrollViewController
+
+class SignUpViewController: ContainerScrollViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.keyboardDismissMode = .interactive
+    }
+
+
+
+
+}
