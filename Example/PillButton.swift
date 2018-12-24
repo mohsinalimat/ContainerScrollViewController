6cf2cf891ac063387023949494f6493909f4e33c
Drew Olbrich <drew.olbrich@gmail.com>
2018-12-24 09:21:30 -0800

    Add PillButton, PillTextField

---
+++ Example/PillButton.swift
@@ -0,0 +1,21 @@
+//
+//  PillButton.swift
+//  Example
+//
+//  Created by Drew Olbrich on 12/24/18.
+//  Copyright © 2018 Drew Olbrich. All rights reserved.
+//
+
+import UIKit
+
+class PillButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Example/PillTextField.swift
@@ -0,0 +1,21 @@
+//
+//  PillTextField.swift
+//  Example
+//
+//  Created by Drew Olbrich on 12/24/18.
+//  Copyright © 2018 Drew Olbrich. All rights reserved.
+//
+
+import UIKit
+
+class PillTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
